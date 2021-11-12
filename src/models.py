16b51898a9aad682c1357ab0e471d1dabb216798
fizteh95 <causeLove95@yandex.ru>
2021-11-12 10:04:41 +0500

    Добавил json.

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 import peewee
 import peewee_async
+from playhouse.postgres_ext import JSONField
 
 # Nothing special, just define model and database:
 
@@ -18,4 +19,36 @@ class BaseModel(peewee.Model):
 class TestModel(BaseModel):
     text = peewee.CharField(max_length=400, default='')
     data = peewee.IntegerField(default=20000)
+
+
+class User(BaseModel):
+    vk_login = peewee.CharField()
+    password = peewee.CharField()
+    tg_id = peewee.IntegerField()
+
+
+class Bot(BaseModel):
+    token = peewee.CharField()
+
+
+class Vk(BaseModel):
+    link = peewee.CharField()
+    last_seen = peewee.DateTimeField()
+
+
+class Tg(BaseModel):
+    channel = peewee.CharField()
+    last_sending = peewee.DateTimeField()
+
+
+class Post(BaseModel):
+    vk_group = peewee.ForeignKeyField(Vk, backref='vk')
+    post_id = peewee.IntegerField()
+    raw_post = JSONField(default={})
+    post_time = peewee.DateTimeField()
+
+
+class UserBot(BaseModel):
+    user = ForeignKeyField(User, backref='user')
+    bot = ForeignKeyField(Bot, backref='favorites')
 
